ed2536ed8e00451e6c26dc6c1c08d6a8e11eae90
jamievullo <jvullo@protonmail.com>
2019-09-02 17:39:00 -0400

    Added check for logged_in to controllers

--- app/controllers/cube_times_controller.rb
+++ app/controllers/cube_times_controller.rb
@@ -1,22 +1,28 @@
 class CubeTimesController < ApplicationController
 
   get "/cube_times" do
-    @cuber = current_cuber
-
-    erb :"/cube_times/index"
+    if logged_in?
+      @cuber = current_cuber
+      erb :"/cube_times/index"
+    else  
+      redirect '/login'
+    end
   end
 
   get "/cube_times/new" do
-    @cube_times = CubeTime.all
-
-    erb :"/cube_times/new"
+    if logged_in?
+      @cube_times = CubeTime.all
+      erb :"/cube_times/new"
+    else  
+      redirect '/login'
+    end
   end
 
   post "/cube_times" do
     if logged_in?
       if params[:cube_time] == ""
 
-        redirect "/cube_times/new"
+        redirect '/cube_times/new'
       else
         @cube_time =  CubeTime.create(params)
         @cube_time.cuber_id = current_cuber.id
@@ -24,11 +30,11 @@ class CubeTimesController < ApplicationController
           
           redirect "/cube_times/#{@cube_time.id}"
         else  
-          redirect "/cube_times/new"
+          redirect '/cube_times/new'
         end
       end
     else
-      redirect "/login"
+      redirect '/login'
     end
   end
 
@@ -37,10 +43,19 @@ class CubeTimesController < ApplicationController
   #   erb :"/cube_times/show"
   # end
 
-  # # GET: /cube_times/5/edit
-  # get "/cube_times/:id/edit" do
-  #   erb :"/cube_times/edit"
-  # end
+  get "/cube_times/:id/edit" do
+    if logged_in?
+      @cube_time = CubeTime.find_by_id(params[:id])
+      if @cube_time && @cube_time.cuber == current_cuber
+        erb :'cube_times/edit_cube_time'
+      else
+        redirect to '/cube_times'
+      end
+    else
+      redirect to '/login'
+    end
+    erb :"/cube_times/edit"
+  end
 
   # # PATCH: /cube_times/5
   # patch "/cube_times/:id" do
