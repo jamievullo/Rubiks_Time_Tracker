dafc09f92badfc28ee4df07ab195faad4e436506
jamievullo <jvullo@protonmail.com>
2019-09-03 13:32:31 -0400

    Added PB and AVG to Cuber Index Page

--- app/controllers/cubers_controller.rb
+++ app/controllers/cubers_controller.rb
@@ -1,12 +1,15 @@
 class CubersController < ApplicationController
 
   get "/cubers/index" do
-    #if logged_in?
+    if logged_in?
     @cuber = current_cuber
-    @cube_time = CubeTime.where("cuber_id = ?", @cuber.id)
-      #binding.pry
-    
+    @cube_times = CubeTime.where("cuber_id = ?", @cuber.id)
+    @best_cube_time = @cube_times.order(cube_time: :asc).first
+    @average_cube_time = @cube_times.sum("cube_time")/@cube_times.count
+    else
+
     erb :"/cubers/index"
+    end 
   end
 
 end
